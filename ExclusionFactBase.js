982686c82f0c7a0e4ee279d05e5c7146a22c2668
John Grey <jgrey@ucsc.edu>
2016-03-06 04:00:54 -0800

    +comments,renamed methods,added subtree retrieval

--- ExclusionFactBase.js
+++ ExclusionFactBase.js
@@ -1,16 +1,31 @@
 /* jshint esversion : 6 */
 
+/**
+   Creates a fact repository / tree
+   @constructor
+   @param {Array.<String>} the strings to assert on construction
+ */
 var ExclusionFactBase = function(...strings){
     this.root = new Map();
     strings.forEach(d=>this.assert(d));
 };
 
-//Tokenize a string
-ExclusionFactBase.prototype.prep = function(string){
+/**
+   Tokenizes an input string for parsing
+   @method
+   @param {String} string The string to tokenize based on '.' and '!'
+   @returns {Array.<String>} tokens
+ */
+ExclusionFactBase.prototype.tokenize = function(string){
     return string.replace(/\./g," $DOT$ ").replace(/!/g," $EX$ ").split(" ").slice(1);
 };
 
-//Parse strings into the fact base
+/**
+   Takes fact strings (.place.people.bob!righthanded) and adds them to the fact base
+   @method
+   @param {Array.<String>} strings The strings to assert
+   @returns {ExclusionFactBase}
+*/
 ExclusionFactBase.prototype.assert = function(...strings){
     "use strict";
     if(strings instanceof Array && strings.length > 1){
@@ -22,8 +37,8 @@ ExclusionFactBase.prototype.assert = function(...strings){
     if(!strings[0].match(/[\.!]/)){
         throw new Error("String should start with a . or !");
     }
-    
-    var tokens = this.prep(strings),
+    //----------
+    var tokens = this.tokenize(strings),
         current = this.root,
         next;
 
@@ -81,11 +96,17 @@ ExclusionFactBase.prototype.assert = function(...strings){
     return this;    
 };
 
+/**
+   Boolean check for if the facts exist in the fact base
+   @constructor
+   @param {Array.<String>} strings The strings to test the fact base for
+   @returns {Boolean} 
+ */
 //test a fact tree of maps for a particular string
-ExclusionFactBase.prototype.test = function(...strings){
+ExclusionFactBase.prototype.exists = function(...strings){
     "use strict";
     if(strings instanceof Array && strings.length > 1){
-        return strings.map(d=>this.test(d)).reduce((m,v)=>m && v,true);
+        return strings.map(d=>this.exists(d)).reduce((m,v)=>m && v,true);
     }else{
         strings = strings.shift();
     }
@@ -96,7 +117,7 @@ ExclusionFactBase.prototype.test = function(...strings){
 
     
     //Single string:
-    var tokens = this.prep(strings),
+    var tokens = this.tokenize(strings),
         current = this.root,
         next;
     
@@ -123,14 +144,20 @@ ExclusionFactBase.prototype.test = function(...strings){
     return true;
 };
 
-//Remove from the fact base a specific pathway
+/**
+   Retracts a single fact chain from the rule base
+   @method
+   @param {String} string
+   @returns {Boolean} the success state of the retraction
+   
+ */
 ExclusionFactBase.prototype.retract = function(string){
     "use strict";
     if(!string[0].match(/[\.!]/)){
         throw new Error("String should start with a . or !");
     }
     
-    var tokens = this.prep(string),
+    var tokens = this.tokenize(string),
         current = this.root,
         next;
 
@@ -161,6 +188,37 @@ ExclusionFactBase.prototype.retract = function(string){
     }
     return false;
 };
+
+/**
+   Gets a subtree out of the fact base
+   @param {String} string The fact string describe where the subtree starts
+   @returns {False || ExclusionFactBase} 
+ */
+ExclusionFactBase.prototype.retrieve = function(string){
+    "use strict";
+    var tokens = this.tokenize(string),
+        current = this.root,
+        next;
+
+    while(tokens.length > 0){
+        next = tokens.shift();
+        if(next.match(/^[(\$DOT\$)(\$EX\$)]/)){
+            next = tokens.shift();
+            if(current.has(next)){
+                current = current.get(next);
+            }else{
+                return false;
+            }
+        }
+    }
+
+    var subFactBase = new ExclusionFactBase();
+    subFactBase.root = current;
+    return subFactBase;    
+};
+
+
+
 
 module.exports = ExclusionFactBase;
 
--- exclusionTests.js
+++ exclusionTests.js
@@ -11,26 +11,26 @@ module.exports = {
 
     simpleAssert : function(test){
         var fb = new EFB();
-        fb.assert(".this.is.a.simple.test");
-        test.ok(fb.root.get('this').get('is').get('a').get('simple').get('test') !== undefined);
+        fb.assert(".this.is.a.simple.exists");
+        test.ok(fb.root.get('this').get('is').get('a').get('simple').get('exists') !== undefined);
         test.done();
     },
 
     simpleTest : function(test){
-        var fb = new EFB(".this.is.a.simple.test");
-        test.ok(fb.test(".this"));
-        test.ok(fb.test(".this.is"));
-        test.ok(fb.test(".this.is.a"));
-        test.ok(fb.test(".this.is.a.simple"));
-        test.ok(fb.test(".this.is.a.simple.test"));
+        var fb = new EFB(".this.is.a.simple.exists");
+        test.ok(fb.exists(".this"));
+        test.ok(fb.exists(".this.is"));
+        test.ok(fb.exists(".this.is.a"));
+        test.ok(fb.exists(".this.is.a.simple"));
+        test.ok(fb.exists(".this.is.a.simple.exists"));
         test.done();
     },
 
     simpleTest_failures : function(test){
-        var fb = new EFB(".this.is.a.simple.test");
-        test.ok(fb.test(".should.fail") === false);
-        test.ok(!fb.test(".this.should.also.fail"));
-        test.ok(!fb.test(".blah.blah.blah"));
+        var fb = new EFB(".this.is.a.simple.exists");
+        test.ok(fb.exists(".should.fail") === false);
+        test.ok(!fb.exists(".this.should.also.fail"));
+        test.ok(!fb.exists(".blah.blah.blah"));
         test.done();
     },
 
@@ -44,60 +44,60 @@ module.exports = {
 
     constructorStringAssertions : function(test){
         var fb = new EFB(".this.is.a.fact.base.entry",".this.is.another.fact.base.entry");
-        test.ok(fb.test(".this.is.a.fact.base.entry"));
-        test.ok(fb.test(".this.is.another.fact.base.entry"));        
+        test.ok(fb.exists(".this.is.a.fact.base.entry"));
+        test.ok(fb.exists(".this.is.another.fact.base.entry"));        
         test.done();
     },
 
     multipleStringsInSingleTest : function(test){
         var fb = new EFB(".this.is.a.fact",".this.is.another.fact",".and.a.third.fact");
-        test.ok(fb.test(".this.is.a.fact"));
-        test.ok(fb.test(".this.is.another.fact"));
-        test.ok(fb.test(".and.a.third.fact"));
+        test.ok(fb.exists(".this.is.a.fact"));
+        test.ok(fb.exists(".this.is.another.fact"));
+        test.ok(fb.exists(".and.a.third.fact"));
         //all together now:
-        test.ok(fb.test(".this.is.a.fact",".this.is.another.fact",".and.a.third.fact"));
+        test.ok(fb.exists(".this.is.a.fact",".this.is.another.fact",".and.a.third.fact"));
         test.done();
     },
 
     multipleStringsInTestWithFailure : function(test){
         var fb = new EFB(".this.is.a.fact",".this.is.another.fact",".and.a.third.fact");
-        test.ok(fb.test(".this.is.a.fact"));
-        test.ok(fb.test(".this.is.another.fact"));
-        test.ok(fb.test(".and.a.third.fact"));
+        test.ok(fb.exists(".this.is.a.fact"));
+        test.ok(fb.exists(".this.is.another.fact"));
+        test.ok(fb.exists(".and.a.third.fact"));
         
-        test.ok(!fb.test(".this.is.not.a.fact"));
-        test.ok(!fb.test(".this.is.a.fact",".this.is.not.a.fact",".and.a.third.fact"));
+        test.ok(!fb.exists(".this.is.not.a.fact"));
+        test.ok(!fb.exists(".this.is.a.fact",".this.is.not.a.fact",".and.a.third.fact"));
         
-        test.ok(fb.test(".this.is.a.fact",".and.a.third.fact"));
+        test.ok(fb.exists(".this.is.a.fact",".and.a.third.fact"));
         test.done();
     },
 
     simpleRetract : function(test){
         var fb = new EFB(".this.is.a.fact");
-        test.ok(fb.test(".this.is.a.fact"));
+        test.ok(fb.exists(".this.is.a.fact"));
         test.ok(fb.retract(".this.is.a"));
         //still exists:
-        test.ok(fb.test(".this.is"));
+        test.ok(fb.exists(".this.is"));
         //doesnt exist:
-        test.ok(!fb.test(".this.is.a"));
-        test.ok(!fb.test(".this.is.a.fact"));
+        test.ok(!fb.exists(".this.is.a"));
+        test.ok(!fb.exists(".this.is.a.fact"));
         test.done();
     },
 
     simpleExclusion : function(test){
-        var fb = new EFB(".this.is.an!exclusion.test");
+        var fb = new EFB(".this.is.an!exclusion.exists");
         //should fail
-        test.ok(!fb.test(".this.is.an.exclusion"));
+        test.ok(!fb.exists(".this.is.an.exclusion"));
         //should pass
-        test.ok(fb.test(".this.is.an!exclusion"));
-        test.ok(fb.test(".this.is.an"));
-        test.ok(fb.test(".this.is.an!exclusion.test"));
+        test.ok(fb.exists(".this.is.an!exclusion"));
+        test.ok(fb.exists(".this.is.an"));
+        test.ok(fb.exists(".this.is.an!exclusion.exists"));
         test.done();
     },
 
     nonExclusiveAssignToRetractedExclusive : function(test){
         var fb = new EFB(".this.is.an!exclusive.fact");
-        test.ok(fb.test(".this.is.an!exclusive.fact"));
+        test.ok(fb.exists(".this.is.an!exclusive.fact"));
 
         //try to assign to 'an'
         test.throws(function(){
@@ -109,9 +109,28 @@ module.exports = {
 
         //now assign successfully to 'an'
         fb.assert(".this.is.an.other");
-        test.ok(fb.test(".this.is.an.other"));
-        test.ok(!fb.test(".this.is.an!exclusive"));
-        test.ok(!fb.test(".this.is.an!exclusive.fact"));
+        test.ok(fb.exists(".this.is.an.other"));
+        test.ok(!fb.exists(".this.is.an!exclusive"));
+        test.ok(!fb.exists(".this.is.an!exclusive.fact"));
+        test.done();
+    },
+
+    subTreeRetrieval : function(test){
+        var fb = new EFB(".this.is.a.set.of.facts",".this.is.another.set.of.facts!subtree1",
+                        ".this.is.another.set.of.side.facts!subtree2");
+        test.ok(fb.exists(".this.is.a.set.of.facts",".this.is.another.set.of.facts!subtree1"));
+        //get the subtree
+        var subTree = fb.retrieve(".this.is.another");
+        test.ok(subTree instanceof EFB);
+        test.ok(subTree.exists(".set.of.facts!subtree1",".set.of.side.facts!subtree2"));
+        //change the original
+        test.ok(fb.retract(".this.is.another.set.of"));
+        //.this.is.another.set should still exist, but nothing more
+        test.ok(!subTree.exists(".set.of.facts!subtree1"));
+        //assert in the subtree, have it reflected in the original
+        subTree.assert(".something.else!blah");
+        test.ok(subTree.exists(".something.else!blah"));
+        test.ok(fb.exists(".this.is.another.something.else!blah"));                       
         test.done();
     },
     
