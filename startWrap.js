a876df39412a12b39f416b0757a34063fd923b22
John Grey <jgrey@ucsc.edu>
2016-04-30 04:41:32 -0700

    adding minify options and related files for minification

---
+++ startWrap.js
@@ -0,0 +1,14 @@
+// wrap-start.frag.js
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(['lodash'], factory);
+    } else if (typeof exports === 'object') {
+        var _ = require('lodash');
+        module.exports = factory(_);
+    } else {
+        // change "myLib" to whatever your library is called
+        root.ANTLR = factory(root._);
+    }
+}(this, function (_) {
+
+    
