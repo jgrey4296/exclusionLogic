b5819c7d2a44bb7eab56fa7ea7a2468f9ba74f18
John Grey <jgrey@ucsc.edu>
2017-03-14 11:35:32 +0000

    initial setup files for webpack, babel, mocha etc

---
+++ webpack.config.babel.js
@@ -0,0 +1,40 @@
+import webpack from 'webpack';
+import path from 'path';
+import FlowBabelWebpackPlugin from 'flow-babel-webpack-plugin';
+
+const ENV = process.env.NODE_ENV || 'development';
+
+module.exports = {
+    context: path.resolve(__dirname,'src'),
+    entry: './BTree.js',
+    
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'BTree.js',
+        library: "BTree",
+        libraryTarget: "umd"        
+    },
+    
+    externals: {
+        "lodash": {
+            commonjs: "lodash",
+            commonjs2: "lodash",
+            amd: "lodash",
+            root: "_",
+        },
+    },
+
+    module:{
+        rules:[
+            {test:/\.js$/, exclude: /node_modules/, enforce: 'pre', loader: 'eslint-loader'},
+            {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+        ]
+    },
+
+    plugins: [
+        new FlowBabelWebpackPlugin(),
+        // new webpack.optimize.UglifyJsPlugin({
+        //     sourceMap: true
+        // })
+    ],
+};
