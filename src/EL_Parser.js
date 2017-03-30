078bc6ffb746cb43dfb2b0e1ff9a7325a028c247
John Grey <jgrey@ucsc.edu>
2017-03-30 09:41:04 +0100

    added some comments, improved query, exported more

--- src/EL_Parser.js
+++ src/EL_Parser.js
@@ -32,22 +32,23 @@ let text = P.regex(/[a-zA-Z_]+/),
     ARR  = P.string('->').result(ELIs.BIND);
 
 
-//something.   something!
+//The final element in a string, doesnt need a bang or dot
 let finalWord = word.map(d=>{ return new ELIs.AccessPair(d); }),
     basePair = P.seqMap(word,P.alt(DOT,BANG),(w,tt) => { return new ELIs.AccessPair(w,tt); }),
-    //something.something!
+    //a string of text+(Bang/Dot) pairs
     baseStr  = basePair.many(),
-    //a.b.c
+    //A Full assert string, from root to base
     assert   = P.seq(P.string('.').then(baseStr), finalWord).map( seq => { return new ELIs.Assertion(_.flatten(seq)); }),
-    //-a.b.c
+    //A full retract string, from root to base
     retract  = P.string('-.').then(P.seq(baseStr,finalWord)).map(seq => { return new ELIs.Retraction(_.flatten(seq)); }),
-	//a.b.c?
-    //todo add negation
-    query    = P.seq(baseStr,finalWord).map( seq => { return new ELIs.Query(_.flatten(seq)); }).skip(QU);
-
+    //A Full query, finishing in ?
+    query    = P.string('.').then(P.seq(baseStr,finalWord)).map( seq => { return new ELIs.Query(_.flatten(seq)); }).skip(QU);
 
 //Main parser:
 //parser ( input : string ) : Base_Instruction
 let ELParser = (P.alt(query,retract,assert).skip(P.alt(P.string('\n'),P.eof,P.string(' ')))).many();
 
 export default ELParser;
+export {
+    assert, retract, query
+};
